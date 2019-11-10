68a156e2b80e5d6d916d6e317bbea088860d8a46
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-11-10 17:49:04 +0300

    first commit

---
+++ TestConnDbWithEf/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestConnDbWithEf.Models;
+
+namespace TestConnDbWithEf.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers() {
+            // Получает данные из БД
+            using (ApplicationContext db = new ApplicationContext()) {
+                var users = db.Users.ToList();
+                foreach (var user in users) {
+                    Console.WriteLine(user.Name);
+                }
+                return await db.Users.ToListAsync();
+            }
+        }
+    }
+}
---
+++ TestConnDbWithEf/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestConnDbWithEf.Models {
+    public class ApplicationContext : DbContext {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext() {
+            // Если БД не создана, то создает ее
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Server=SIERRAF4C1;Database=TestDB;Trusted_Connection=True;");
+        }
+    }
+}
